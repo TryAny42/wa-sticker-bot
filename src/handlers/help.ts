09a4d7ca26559e27729d89fc4f5df39fd43c8c7f
TryAny42 <139024841+TryAny42@users.noreply.github.com>
2023-08-04 14:01:35 +0700

    Update help.ts

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -2,8 +2,14 @@ import { Message } from 'whatsapp-web.js';
 
 const helpHandler = (message: Message) => {
   message.reply(
-    `*User guide*
-Kirim gambar dengan deskripsi sebagai berikut:
+    `Selamat datang di Layanan Informasi *Desa Mendolo Lor*
+Ketik angka untuk memilih layanan kami:
+
+1. Pemerintah Desa
+2. Kegiatan Desa Bulan Ini
+3. Data UMKM
+4. Tata Cara Permohonan Surat
+5. Website Desa
 
 *Buat stiker biasa*
 \`\`\`!sticker\`\`\`

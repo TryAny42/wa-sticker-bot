90fe76d42aa4c5fbdd81927e7b964012c5f3d958
TryAny42 <139024841+TryAny42@users.noreply.github.com>
2023-08-06 11:29:46 +0700

    Update event.ts

--- src/handlers/event.ts
+++ src/handlers/event.ts
@@ -1,1 +1,12 @@
+import { Message } from 'whatsapp-web.js';
 
+const eventHandler = (message: Message) => {
+  message.reply(`Kegiatan Desa Mendolo Lor bulan Agustus 2023
+  
+  1. 
+  2. 
+  3. `
+  );
+};
+
+export default eventHandler;

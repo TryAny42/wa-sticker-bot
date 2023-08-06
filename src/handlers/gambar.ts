046090fc6f1760ab08543b25156bcd4e72d0ed66
TryAny42 <139024841+TryAny42@users.noreply.github.com>
2023-08-06 15:53:48 +0700

    Create gambar.ts

---
+++ src/handlers/gambar.ts
@@ -0,0 +1,10 @@
+import { Message } from 'whatsapp-web.js';
+
+const media = await Message.fromUrl('https://github.com/TryAny42/wa-sticker-bot/blob/ceef91623f498919b091e84813963d3af484595d/assets/img/umkm.PNG');
+
+const gambarHandler = (message: Message) => {
+  message.reply(media
+  );
+};
+
+export default gambarHandler;

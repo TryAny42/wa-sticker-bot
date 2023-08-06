a6ee42eb25eebed399647401093b406fd298b8b1
TryAny42 <139024841+TryAny42@users.noreply.github.com>
2023-08-06 10:53:41 +0700

    Update website.ts

--- src/handlers/website.ts
+++ src/handlers/website.ts
@@ -1,1 +1,9 @@
+import { Message } from 'whatsapp-web.js';
 
+const helpHandler = (message: Message) => {
+  message.reply(`https://mendololor.kabpacitan.id/
+  Buka website Desa Mendolo Lor untuk mendapatkan informasi terbaru desa`
+  );
+};
+
+export default websiteHandler;

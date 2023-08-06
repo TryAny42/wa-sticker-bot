9f7e85eb04d004805a5dcb3647783589607d7e01
TryAny42 <139024841+TryAny42@users.noreply.github.com>
2023-08-06 10:17:26 +0700

    Update help.ts

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -10,24 +10,6 @@ Ketik angka untuk memilih layanan kami:
 3. Data UMKM
 4. Tata Cara Permohonan Surat
 5. Website Desa
-
-*Buat stiker biasa*
-\`\`\`!sticker\`\`\`
-
-*Buat stiker dengan custom name*
-\`\`\`!sticker name="Nama stiker"\`\`\`
-
-*Buat stiker dengan custom author*
-\`\`\`!sticker author="Nama author"\`\`\`
-
-*Buat stiker dengan custom name dan author*
-\`\`\`!sticker name="Nama stiker" author="Nama author"\`\`\`
-
-*Tampilkan user guide*
-\`\`\`!help\`\`\`
-
-
-_Saat ini bot belum mendukung gambar png_`
   );
 };
 

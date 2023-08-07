cb847f4126f368afb8f5f88028604a6e395c8598
TryAny42 <139024841+TryAny42@users.noreply.github.com>
2023-08-07 09:09:48 +0700

    Update gambar.ts

--- src/handlers/gambar.ts
+++ src/handlers/gambar.ts
@@ -1,12 +1,33 @@
 import { Message,MessageMedia } from 'whatsapp-web.js';
 
-const media = async () => {
+import goErrorHandler from '../utils/goErrHandler';
+import printLog from '../utils/logger';
+
+/*const media = async () => {
         await MessageMedia.fromUrl('https://github.com/TryAny42/wa-sticker-bot/blob/ceef91623f498919b091e84813963d3af484595d/assets/img/umkm.PNG');
 };
 
 const gambarHandler = (message: Message) => {
   message.reply(media, message.from
   );
+};*/
+
+const gambarHandler = async ({
+  message,
+}: GambarHandlerParams) => {
+        
+  const media = async () => {
+        await MessageMedia.fromUrl('https://github.com/TryAny42/wa-sticker-bot/blob/ceef91623f498919b091e84813963d3af484595d/assets/img/umkm.PNG');
+  };
+  
+  const { error: replyError } = await goErrorHandler(() =>
+    message.reply(media, message.from);
+  );
+  if (replyError instanceof Error) {
+    message.reply('Terjadi kesalahan pada saat mengirim gambar');
+    return console.error('Error when sending sticker.', replyError);
+  }
 };
+
 
 export default gambarHandler;

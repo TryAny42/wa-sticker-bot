1e894418e3b3795b22523a67eb462deb087b2bab
TryAny42 <139024841+TryAny42@users.noreply.github.com>
2023-08-07 08:15:08 +0700

    Update gambar.ts

--- src/handlers/gambar.ts
+++ src/handlers/gambar.ts
@@ -1,9 +1,9 @@
 import { Message } from 'whatsapp-web.js';
 
-const media = await Message.fromUrl('https://github.com/TryAny42/wa-sticker-bot/blob/ceef91623f498919b091e84813963d3af484595d/assets/img/umkm.PNG');
+//const media = await Message.fromUrl('https://github.com/TryAny42/wa-sticker-bot/blob/ceef91623f498919b091e84813963d3af484595d/assets/img/umkm.PNG');
 
 const gambarHandler = (message: Message) => {
-  message.reply(media
+  message.reply(`media`
   );
 };
 

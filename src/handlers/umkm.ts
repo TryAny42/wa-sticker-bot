f6b616d16ff75e2913fa298f021bfa5d9095e35f
TryAny42 <139024841+TryAny42@users.noreply.github.com>
2023-08-07 20:13:50 +0700

    Update umkm.ts

--- src/handlers/umkm.ts
+++ src/handlers/umkm.ts
@@ -1,9 +1,24 @@
-import { Message } from 'whatsapp-web.js';
+import { Message,MessageMedia } from 'whatsapp-web.js';
 
-const umkmHandler = (message: Message) => {
+import goErrorHandler from '../utils/goErrHandler';
+
+const umkmHandler = async (message: Message) => {
+        
+  const media = await MessageMedia.fromUrl('https://i.ibb.co/VMzbfqy/umkm.png');
+  
+  const { error: replyError } = await goErrorHandler(() =>
+    message.reply(media, message.from)
+  );
+  if (replyError instanceof Error) {
+    message.reply('Terjadi kesalahan pada saat mengirim gambar');
+    return console.error('Error when sending sticker.', replyError);
+  }
+};
+
+/*const umkmHandler = (message: Message) => {
   message.reply(
     `Berikut data UMKM yang ada di Mendolo Lor:
-
+/*
 Krajan
 1  |   Keripik Singkong    |    Ria Indah    | 085800053183 |
 2  |  Keripik tempe Sagu   |  Wingky Ananta  | 083846238404 |
@@ -30,6 +45,6 @@ Kembang
 13 | Kusen, Anyaman, Areng |      Mulyono    |       -      |
 14 |     Industri Kayu     |  Slamet Riyadi  |       -      |`
   );
-};
+};*/
 
 export default umkmHandler;

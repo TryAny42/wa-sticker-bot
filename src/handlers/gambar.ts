7a71ec8f2c5cf7d301c73feeb21dfb492f5a5bab
TryAny42 <139024841+TryAny42@users.noreply.github.com>
2023-08-07 09:54:29 +0700

    Update gambar.ts

--- src/handlers/gambar.ts
+++ src/handlers/gambar.ts
@@ -26,7 +26,7 @@ const gambarHandler = async (message: Message) => {
   //const media = async () => {
   //      await MessageMedia.fromUrl('https://github.com/TryAny42/wa-sticker-bot/blob/ceef91623f498919b091e84813963d3af484595d/assets/img/umkm.PNG');
   //};
-  const media = await MessageMedia.fromUrl('https://github.com/TryAny42/wa-sticker-bot/blob/ceef91623f498919b091e84813963d3af484595d/assets/img/umkm.PNG');
+  const media = await MessageMedia.fromUrl('https://via.placeholder.com/350x150.png');
   
   const { error: replyError } = await goErrorHandler(() =>
     message.reply(media, message.from)

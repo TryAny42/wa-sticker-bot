28c35a92821ab6d509ff0fa25c9d1ea05ae878c4
TryAny42 <139024841+TryAny42@users.noreply.github.com>
2023-08-11 22:10:54 +0700

    Update desa.ts

--- src/handlers/desa.ts
+++ src/handlers/desa.ts
@@ -4,10 +4,22 @@ const desaHandler = (message: Message) => {
   message.reply(
     `Ketik *huruf* untuk mendapatkan kontak perangkat desa:
 
-A. Kepala Desa
-B. Sekretaris Desa
-C. Kepala Urusan
-D. Kepala Dusun`
+A. Badan Permusyawaratan Desa		
+B. Kepala Desa/Lurah			
+C. Sekretaris Desa		
+D. Kasi Pemerintahan		
+E. Kasi Pembangunan			
+F. Kasi Kesejahteraan Rakyat	
+G. Kasi Urusan Umum		
+H. Kasi Urusan Keuangan		
+I. Kasi Perencanaan		
+J. Kepala Dusun Krajan
+K. Kepala Dusun Kembang	
+L. Kepala Dusun Pagerjo	
+M. Kepala Dusun Pangkah	
+N. Kepala Dusun Ngaglik	
+O. Kepala Dusun Dukun	
+P. Kepala Dusun Ngepoh`
   );
 };
 

40e0b7d7ca16bf905721816ea148bd56bfb8a2b6
TryAny42 <139024841+TryAny42@users.noreply.github.com>
2023-08-25 10:56:35 +0700

    Update surat.ts

--- src/handlers/surat.ts
+++ src/handlers/surat.ts
@@ -4,7 +4,7 @@ const suratHandler = (message: Message) => {
   message.reply(`Pembuatan surat di Desa Mendolo Lor dapat dilakukan dengan dua cara, yaitu datang langsung ke Kantor Desa Mendolo Lor dan melakukan permohonan melalui website Desa
   
 Berikut tata cara untuk melakukan permohonan surat melalui website desa:
-1.
+1. 
 2.
 3.`
   );

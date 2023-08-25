3124da9ce267e52db232595ac642b38074197877
TryAny42 <139024841+TryAny42@users.noreply.github.com>
2023-08-25 10:38:24 +0700

    Update desa.ts

--- src/handlers/desa.ts
+++ src/handlers/desa.ts
@@ -2,24 +2,24 @@ import { Message } from 'whatsapp-web.js';
 
 const desaHandler = (message: Message) => {
   message.reply(
-    `Ketik *huruf* untuk mendapatkan kontak perangkat desa:
+    `Perangkat Desa Mendolo Lor:
 
-A. Badan Permusyawaratan Desa		
-B. Kepala Desa/Lurah			
-C. Sekretaris Desa		
-D. Kasi Pemerintahan		
-E. Kasi Pembangunan			
-F. Kasi Kesejahteraan Rakyat	
-G. Kasi Urusan Umum		
-H. Kasi Urusan Keuangan		
-I. Kasi Perencanaan		
-J. Kepala Dusun Krajan
-K. Kepala Dusun Kembang	
-L. Kepala Dusun Pagerjo	
-M. Kepala Dusun Pangkah	
-N. Kepala Dusun Ngaglik	
-O. Kepala Dusun Dukun	
-P. Kepala Dusun Ngepoh`
+A. Badan Permusyawaratan Desa : Sukirman
+B. Kepala Desa/Lurah			    : Winarno
+C. Sekretaris Desa		        : Teguh Wahyono
+D. Kasi Pemerintahan		      : S. Harianto
+E. Kasi Pembangunan			      : Suwadu
+F. Kasi Kesejahteraan Rakyat	: Imam Ahmadi
+G. Kasi Urusan Umum		        : Supriyanto
+H. Kasi Urusan Keuangan		    : S. Indra Wibowo
+I. Kasi Perencanaan		        : Supriyadi
+J. Kepala Dusun Krajan        : Sumarno
+K. Kepala Dusun Kembang	      : Joko Priyono
+L. Kepala Dusun Pagerjo	      : Suranto
+M. Kepala Dusun Pangkah	      : Tumadi
+N. Kepala Dusun Ngaglik	      : Suyadi
+O. Kepala Dusun Dukun	        : Sumarno
+P. Kepala Dusun Ngepoh        : Joko Susilo` 
   );
 };
 
